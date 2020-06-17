39f7cc2b1b7d7346402ff368907e50151008483a
moshicode <moshikoshamir@live.com>
2020-06-17 18:25:50 +0300

    Updates

---
+++ precache-manifest.ebfdbda411bdef2968775197e72b0036.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c115233325cf67a440e1b85f7cc8f14",
+    "url": "/travel/index.html"
+  },
+  {
+    "revision": "0723d124d9a6327b9128",
+    "url": "/travel/static/css/main.8982c2ec.chunk.css"
+  },
+  {
+    "revision": "471811ad255c7f29c61c",
+    "url": "/travel/static/js/2.cbf9d2f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/travel/static/js/2.cbf9d2f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0723d124d9a6327b9128",
+    "url": "/travel/static/js/main.d883e969.chunk.js"
+  },
+  {
+    "revision": "9afaa8a9512212073e15",
+    "url": "/travel/static/js/runtime-main.487f553e.js"
+  }
+]);
